21d08f4a4b9702f2b83cd74a64524eedf70a49ac
Eric Seidel <eseidel2004@gmail.com>
2026-05-24 17:37:38 +0200

    fix: format playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,55 +1,55 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 const crossBrowser = !!process.env.PLAYWRIGHT_CROSS_BROWSER;
 const projects = crossBrowser
   ? [
       {
-        name: 'chromium',
-        use: { ...devices['Desktop Chrome'] },
+        name: "chromium",
+        use: { ...devices["Desktop Chrome"] },
       },
       {
-        name: 'firefox',
-        use: { ...devices['Desktop Firefox'] },
+        name: "firefox",
+        use: { ...devices["Desktop Firefox"] },
       },
       {
-        name: 'webkit',
-        use: { ...devices['Desktop Safari'] },
+        name: "webkit",
+        use: { ...devices["Desktop Safari"] },
       },
       {
-        name: 'Mobile Chrome',
-        use: { ...devices['Pixel 5'] },
+        name: "Mobile Chrome",
+        use: { ...devices["Pixel 5"] },
       },
       {
-        name: 'Mobile Safari',
-        use: { ...devices['iPhone 12'] },
+        name: "Mobile Safari",
+        use: { ...devices["iPhone 12"] },
       },
     ]
   : [
       {
-        name: 'chromium',
-        use: { ...devices['Desktop Chrome'] },
+        name: "chromium",
+        use: { ...devices["Desktop Chrome"] },
       },
       {
-        name: 'Mobile Chrome',
-        use: { ...devices['Pixel 5'] },
+        name: "Mobile Chrome",
+        use: { ...devices["Pixel 5"] },
       },
     ];
 
 export default defineConfig({
-  testDir: './__tests__/e2e',
+  testDir: "./__tests__/e2e",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'line',
+  reporter: "line",
   use: {
-    baseURL: 'http://localhost:3000',
-    trace: 'on-first-retry',
+    baseURL: "http://localhost:3000",
+    trace: "on-first-retry",
   },
   projects,
   webServer: {
-    command: 'npm run dev -- --hostname 127.0.0.1 --port 3000',
-    url: 'http://localhost:3000',
+    command: "npm run dev -- --hostname 127.0.0.1 --port 3000",
+    url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
   },
